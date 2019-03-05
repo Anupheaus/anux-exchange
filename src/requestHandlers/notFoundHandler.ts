0fd86dfd08ac972071fb4d55552e6494079b817c
Anupheaus <tony.hales@moodinternational.com>
2019-03-05 08:29:40 +0000

    Switched error being thrown from the notFoundHandler

--- src/requestHandlers/notFoundHandler.ts
+++ src/requestHandlers/notFoundHandler.ts
@@ -1,9 +1,8 @@
-import { BadRequestError } from '../errors';
+import { NotFoundError } from '../errors';
 import { Request } from 'express';
 
 export function notFoundHandler() {
   return (req: Request) => {
-    const error = new BadRequestError('No valid route was found for this request.', { method: req.method, path: req.path });
-    throw error;
+    throw new NotFoundError('No valid route was found for this request.', { method: req.method, path: req.path });
   };
 }
