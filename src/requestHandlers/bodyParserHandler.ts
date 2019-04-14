836af3cffff15b6b7d90272dfbbdb1aa7115e049
Anupheaus <tony.hales@moodinternational.com>
2019-04-14 22:57:29 +0100

    Changed request and response to be express interfaces for bodyParserHandler

--- src/requestHandlers/bodyParserHandler.ts
+++ src/requestHandlers/bodyParserHandler.ts
@@ -1,12 +1,11 @@
 import * as bodyParser from 'body-parser';
-import { IncomingMessage, ServerResponse } from 'http';
-import { NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 /**
  * Enables the parsing of JSON request bodies.
  */
 export function bodyParserHandler() {
-  return async (req: IncomingMessage, res: ServerResponse, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     bodyParser.json({})(req, res, () => {
       bodyParser.urlencoded({ extended: false })(req, res, next);
     });
